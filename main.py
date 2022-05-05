9699d0a99266de18923fd57804f5014057c5cc3b
Alan <elanworld@163.com>
2022-05-05 16:09:58 +0800

    添加homeassistant时长上报

--- main.py
+++ main.py
@@ -3,17 +3,19 @@ import ctypes
 import sys
 import time
 
-import PySimpleGUI as sg
+import PySimpleGUI as Sg
 
+from common.mqtt_utils import MqttBase
+from tools.server_box.homeassistant_mq_entity import HomeAssistantEntity
 from common import python_box
 import keyboard
 
 
-def lock_screen(duration=0.2, passwd=None, **kwargs) -> bool:
+def lock_screen(duration=0.2, _passwd=None, **kwargs) -> bool:
     """
     锁屏ui
     :param duration:
-    :param passwd:
+    :param _passwd:
     :param kwargs:
     :return: 是否密码解锁
     """
@@ -21,23 +23,23 @@ def lock_screen(duration=0.2, passwd=None, **kwargs) -> bool:
     bk_color = "#3C3F41"
     user32 = ctypes.windll.user32
     screensize = user32.GetSystemMetrics(0), user32.GetSystemMetrics(1)
-    timer = sg.Text(time.strftime("%Y-%m-%d", time.localtime()),
+    timer = Sg.Text(time.strftime("%Y-%m-%d", time.localtime()),
                     key="_time_",
                     justification='center',
                     font=f"any {int(screensize[0] / 10)}",
                     background_color=bk_color
                     )
-    gui_input = sg.Input(key=key_input, password_char="*")
+    gui_input = Sg.Input(key=key_input, password_char="*")
     column = [
         [timer, ],
         [gui_input, ],
     ]
-    layout = [[sg.VPush(background_color=bk_color)],
-              [sg.Push(background_color=bk_color),
-               sg.Column(column, element_justification='c', background_color=bk_color),
-               sg.Push(background_color=bk_color)],
-              [sg.VPush(background_color=bk_color)]]
-    window = sg.Window('Stand Up And Drink Water',
+    layout = [[Sg.VPush(background_color=bk_color)],
+              [Sg.Push(background_color=bk_color),
+               Sg.Column(column, element_justification='c', background_color=bk_color),
+               Sg.Push(background_color=bk_color)],
+              [Sg.VPush(background_color=bk_color)]]
+    window = Sg.Window('Stand Up And Drink Water',
                        layout,
                        no_titlebar=True,
                        keep_on_top=True,
@@ -49,14 +51,14 @@ def lock_screen(duration=0.2, passwd=None, **kwargs) -> bool:
                        size=screensize
                        )
     start = time.time()
-    keyboard.hook_key("windows", lambda x : print(x), True)
+    keyboard.hook_key("windows", lambda x: print(x), True)
     while True:
         event, values = window.Read(timeout=300)
         if time.time() - start > duration * 60:
             window.close()
             keyboard.unhook_key("windows")
             return False
-        if values.get(key_input) == passwd:
+        if values.get(key_input) == _passwd:
             window.close()
             keyboard.unhook_key("windows")
             return True
@@ -76,12 +78,23 @@ if __name__ == '__main__':
     day_config = python_box.read_config("%s" % config_log_ini, {("%s" % day_time): 0, ("%s" % today): _get_today()})
     day_limit = "day_limit"
     passwd = "passwd"
+    message = "send message"
+    host = "mq host"
+    port = "mq port"
     config = python_box.read_config("config/config.ini",
                                     {lock_time: 5, ("%s" % unlock_time): 25, ("%s" % loop): 48, ("%s" % passwd): 123,
-                                     ("%s" % day_limit): 100}, )
+                                     ("%s" % day_limit): 100,
+                                     ("%s" % message): "0 #是否发送消息1 0", ("%s" % host): "www.xianneng.top",
+                                     ("%s" % port): "8883"}, )
     if not config:
         print("请配置并重新运行")
         sys.exit(0)
+    entity = None
+    if config.get(message) == "1":
+        mq = MqttBase(config.get(host), int(config.get(port)))
+        mq.connect()
+        entity = HomeAssistantEntity(mq)
+        entity.config_topic("day_use", "当日使用时长")
     for _ in range(int(config.get(loop))):
         first_run = None
         use_passwd = None
@@ -91,7 +104,7 @@ if __name__ == '__main__':
             day_config[day_time] = 0
             first_run = True
         if not first_run:
-            use_passwd = lock_screen(duration=float(config.get(lock_time)), passwd=config.get(passwd))
+            use_passwd = lock_screen(duration=float(config.get(lock_time)), _passwd=config.get(passwd))
             first_run = False
         overtime = float(day_config.get(day_time)) >= float(config.get(day_limit))
         if not overtime or use_passwd or (overtime and use_passwd):
@@ -99,3 +112,5 @@ if __name__ == '__main__':
             time.sleep(r * 60)
             day_config[day_time] = float(day_config.get(day_time)) + r
         python_box.write_config(day_config, config_log_ini)
+        entity.send_state(f"{day_config.get(day_time)} 分钟") if entity else None
+    entity.mq.close() if entity else None
